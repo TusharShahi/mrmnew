d9125727e3023ffb30775150d6950a064a564bb7
tusharsingh8 <tushki.shahi@gmail.com>
2017-05-26 17:32:42 +0530

    First commit

---
+++ score.py
@@ -0,0 +1,6 @@
+import ctypes
+import requests
+from bs4 import BeautifulSoup
+from google import search
+x = s.get('http://www.goal.com/en-gb/live-scores?ICID=MP_TN_7_1')
+ctypes.windll.user32.MessageBoxA(None,'Hello','Match Score',0)
